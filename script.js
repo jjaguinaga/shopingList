9ac65671a1976b20fda2025a04726c566122362b
Juan Aguinaga <juan_a03@icloud.com>
2024-09-24 22:44:55 -0500

    added the clear function

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
+const clearButton = document.getElementById('clear');
 
 function addItem(e) {
    e.preventDefault();
@@ -34,4 +35,18 @@ function createIcon(classes) {
    return icon;
 }
 
+function removeItem(e) {
+   if (e.target.parentElement.classList.contains('remove-item')) {
+      e.target.parentElement.parentElement.remove();
+   }
+}
+
+function clearItems(e) {
+   while (itemList.firstChild) {
+      itemList.removeChild(itemList.firstChild);
+   }
+}
+
 itemForm.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+clearButton.addEventListener('click', clearItems);
