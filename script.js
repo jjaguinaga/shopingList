b77dc0e0779cba23ae69c4758d458e2179c43a00
Juan Aguinaga <juan_a03@icloud.com>
2024-09-24 21:35:06 -0500

    got rid of notes

--- script.js
+++ script.js
@@ -11,7 +11,6 @@ function addItem(e) {
       return;
    }
 
-   // create a new item for list
    const li = document.createElement('li');
    li.appendChild(document.createTextNode(newItem));
 
@@ -20,8 +19,6 @@ function addItem(e) {
    itemList.appendChild(li);
    itemInput.value = '';
 }
-// once the button and icon are created from the two functions below
-// you need to append them to the list to show on the DOM
 
 function createButton(classes) {
    const button = document.createElement('button');
@@ -30,15 +27,11 @@ function createButton(classes) {
    button.appendChild(icon);
    return button;
 }
-// this function is used to make the button for the new item list
-// the icon function is used here to add in the icon
 
 function createIcon(classes) {
    const icon = document.createElement('i');
    icon.className = classes;
    return icon;
 }
-// made this function to create the icon for the button
-// then we add it inside the function for the button
 
 itemForm.addEventListener('submit', addItem);
