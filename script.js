d477056774e36138d94e6a2aad3274141b609893
Juan Aguinaga <juan_a03@icloud.com>
2024-09-24 23:21:10 -0500

    removed the button and filter

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const itemForm = document.getElementById('item-form');
 const itemInput = document.getElementById('item-input');
 const itemList = document.getElementById('item-list');
 const clearButton = document.getElementById('clear');
+const itemFilter = document.getElementById('filter');
+const items = itemList.querySelectorAll('li');
 
 function addItem(e) {
    e.preventDefault();
@@ -18,6 +20,7 @@ function addItem(e) {
    const button = createButton('remove-item btn-link text-red');
    li.appendChild(button);
    itemList.appendChild(li);
+   checkUI();
    itemInput.value = '';
 }
 
@@ -38,6 +41,7 @@ function createIcon(classes) {
 function removeItem(e) {
    if (e.target.parentElement.classList.contains('remove-item')) {
       e.target.parentElement.parentElement.remove();
+      checkUI();
    }
 }
 
@@ -45,8 +49,22 @@ function clearItems(e) {
    while (itemList.firstChild) {
       itemList.removeChild(itemList.firstChild);
    }
+   checkUI();
+}
+
+function checkUI() {
+   const items = itemList.querySelectorAll('li');
+   if (items.length === 0) {
+      clearButton.style.display = 'none';
+      itemFilter.style.display = 'none';
+   } else {
+      clearButton.style.display = 'block';
+      itemFilter.style.display = 'block';
+   }
 }
 
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearButton.addEventListener('click', clearItems);
+
+checkUI();
